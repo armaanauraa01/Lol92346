d5d5774f318462234571c80e645756a327beec7d
armaanauraa01 <banokhatoon666@gmail.com>
2026-07-04 14:00:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Welcome to Arman Gaming Website!");
+document.addEventListener("DOMContentLoaded", () => {
+
+    alert("🔥 Welcome To Arman Gaming AI Hub 🔥");
+
+    console.log("Website Loaded Successfully");
+
+    const links = document.querySelectorAll("a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function () {
+            console.log("Open:", this.innerText);
+        });
+    });
+
+});
